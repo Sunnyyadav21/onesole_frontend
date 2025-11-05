bce543c99f6e19923d0a9c5857bd690a5484fd55
Sunnyyadav21 <my191141@gmail.com>
2025-11-05 20:55:21 +0530

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
-  plugins: [react(),],
-  define: { 'process.env': {}, },
+  plugins: [react()],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  server: {
+    allowedHosts: ['onesoulcare.in'], // ✅ allow your domain
   },
 })
